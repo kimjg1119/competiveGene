6e7f57aa6dfbcc7356ab1a9dd69a6a6352fdb4b8
김준겸 <kimjg1199@naver.com>
2015-05-03 13:49:16 +0900

    #150503 main 함수 생성, 랜덤 클래스 생성, Gene 클래스 생성중

---
+++ competieveGene/src/competieveGene/CompetiveGene.java
@@ -0,0 +1,43 @@
+package competieveGene;
+import java.util.*;
+
+class MakeRandom
+{
+	private Random ra=new Random();
+	private int dice(int a, int b)
+	{
+		return 1+ra.nextInt(b-a);
+	}
+	public int[] dicePack(int a, int b, int m)
+	{
+		int i;
+		int[] diceArr=new int[m];
+		for(i=0;i<m;i++)
+		{
+			diceArr[i]=dice(a,b);
+		}
+		
+		return diceArr;
+	}
+}
+
+class Gene
+{
+	private int[] mygene=new int[1000];
+	private int geneNum;
+	
+	public Gene(int geneNumTemp)
+	{
+		int[] arr=new int[geneNumTemp];
+		System.arraycopy(mygene, 0, arr, 0, geneNumTemp);
+	}
+}
+
+public class CompetiveGene {
+
+	public static void main(String[] args) {
+		
+		
+	}
+
+}
